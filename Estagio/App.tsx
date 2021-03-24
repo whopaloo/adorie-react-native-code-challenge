a3d0386c887a35326f0a2f01c8c08eefa6c9a944
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-24 00:54:38 -0300

    adicionado Navigator e Screen do react-navigation

--- Estagio/App.tsx
+++ Estagio/App.tsx
@@ -8,17 +8,24 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
-import {Text, View} from 'react-native';
 import {myTheme} from './my-theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Home} from './screens/home.screen';
+
+const AppStack = createStackNavigator();
 
 const App = () => {
   return (
     <ThemeProvider theme={myTheme}>
-      <View>
-        <Text>Hello</Text>
-      </View>
+      <NavigationContainer>
+        <AppStack.Navigator>
+          <AppStack.Screen name="Home" component={Home} />
+        </AppStack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
